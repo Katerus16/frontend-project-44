f17bdfb7d35737ca8d0fbeb03aba98967b3b81ae
katerus16 <sherryyy@yandex.ru>
2024-07-30 23:43:38 +0300

    install eslint

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Katerus16/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Katerus16/frontend-project-44/actions)
+<a href="https://codeclimate.com/github/Katerus16/frontend-project-44/maintainability"><img src="https://api.codeclimate.com/v1/badges/e8a4388c3b43ee8fe316/maintainability" /></a>
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const nameQuestion = () => {
-    console.log("Welcome to the Brain Games!");
-    const answer = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${answer}!`);
+const nameQuestion = () => {
+  console.log('Welcome to the Brain Games!');
+  const answer = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${answer}!`);
 };
+export default nameQuestion;
